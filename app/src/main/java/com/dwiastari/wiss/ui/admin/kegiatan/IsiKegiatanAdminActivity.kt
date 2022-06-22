97f8611ed813a66d87ac3e7f09201cd7e16334bd
Dwi Astari <dwi18si@mahasiswa.pcr.ac.id>
2022-06-22 13:25:34 +0700

    done bismillah ya

--- app/src/main/java/com/dwiastari/wiss/ui/admin/kegiatan/IsiKegiatanAdminActivity.kt
+++ app/src/main/java/com/dwiastari/wiss/ui/admin/kegiatan/IsiKegiatanAdminActivity.kt
@@ -176,8 +176,7 @@ class IsiKegiatanAdminActivity : AppCompatActivity() {
         val year = c.get(Calendar.YEAR)
         val month = c.get(Calendar.MONTH)
         val day = c.get(Calendar.DAY_OF_MONTH)
-    
-    
+
         val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
         
             // Display Selected date in textbox
